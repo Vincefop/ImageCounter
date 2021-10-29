1fd326fd9f2a86843e9daeeeecc0b70d8ee86673
Vincent <vincent.debuisson@gmail.com>
2021-10-29 11:29:33 +0200

    first commit

---
+++ src/test/LoadImageApp.java
@@ -0,0 +1,102 @@
+package test;
+
+
+/*
+ * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
+ 
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+import javax.swing.*;
+ 
+/**
+ * This class demonstrates how to load an Image from an external file
+ */
+public class LoadImageApp extends Component {
+          
+	/**
+	 * L'image bufferisée = l'image mise sous forme d'un grand tableau
+	 */
+    BufferedImage img;
+ 
+    /**
+     * ??
+     */
+    public void paint(Graphics g) {
+        g.drawImage(img, 0, 0, null);
+    }
+ 
+    /**
+     * Récupérer une image et la mettre dans l'image bufferisée
+     */
+    public LoadImageApp() {
+       try {
+    	   //mettre une image dans une variable bufferisée img
+           img = ImageIO.read(new File("C:/dev/WorkspaceFormationJava/OrganismCounter/src/test/strawberry.jpg"));
+       } catch (IOException e) {//exception sur l'ouverture et la fermeture d'un fichier
+       }
+ 
+    }
+ 
+    /**
+     * redimensionnement
+     */
+    public Dimension getPreferredSize() {
+        if (img == null) {
+             return new Dimension(100,100);
+        } else {
+           return new Dimension(img.getWidth(null), img.getHeight(null));
+       }
+    }
+ 
+    /**
+     * la méthode main 
+     * @param args
+     */
+    public static void main(String[] args) {
+    	// la fenêtre sur laquelle on pose la photo
+        JFrame f = new JFrame("Load Image Sample");
+            
+        //quand on ferme la fenêtre ça ferme le programme
+        f.addWindowListener(new WindowAdapter(){
+                public void windowClosing(WindowEvent e) {
+                    System.exit(0);//se ferme à la fin et sort
+                }
+            });
+        //sur la frame on ajoute la photo
+        f.add(new LoadImageApp());
+        f.pack();
+        f.setVisible(true);
+    }
+}
